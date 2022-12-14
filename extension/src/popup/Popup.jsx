471a4cd61e62d2b84b386a0c0600379e2c733eff
Noorvir Aulakh <noorvir.aulakh@gmail.com>
2022-12-14 17:01:47 +0100

    Init working global shortcut

--- extension/src/popup/Popup.jsx
+++ extension/src/popup/Popup.jsx
@@ -1,46 +1,61 @@
-import '@picocss/pico'
-import { useCallback, useEffect, useState } from 'preact/hooks'
-import { getUserConfig, updateUserConfig, TRIGGER_MODES } from '../config'
-import './styles.css'
+import "@picocss/pico";
+
+import {useCallback, useEffect, useState} from "preact/hooks";
+import {getUserConfig, updateUserConfig} from "../config";
+import "./styles.css";
+
+// function SpeechInput() {
+//   const [transcript, setTranscript] = useState("");
+//
+//   const client = new SpeechClient();
+//   const request = {
+//     config: {
+//       encoding: "LINEAR16",
+//       sampleRateHertz: 16000,
+//       languageCode: "en-US",
+//     },
+//   };
+//
+//   const microphone = new MicrophoneStream();
+//   microphone.pipe(request);
+//
+//   // Create a readable stream for the transcription
+//   client.streamingRecognize(request).on("data", (data) => {
+//     // When data is received from the stream, append the transcription to the text box
+//     setTranscript(transcript + data.results[0].alternatives[0].transcript);
+//   });
+//
+//   return <textarea value={transcript} />;
+// }
 
 function Popup() {
-  const [triggerMode, setTriggerMode] = useState()
+  const [triggerMode, setTriggerMode] = useState();
 
   useEffect(() => {
     getUserConfig().then((config) => {
-      setTriggerMode(config.triggerMode || 'always')
-    })
-  }, [])
+      setTriggerMode(config.triggerMode || "always");
+    });
+  }, []);
 
   const onTriggerModeChange = useCallback((e) => {
-    const mode = e.target.value
-    setTriggerMode(mode)
-    updateUserConfig({ triggerMode: mode })
-  }, [])
+    const mode = e.target.value;
+    setTriggerMode(mode);
+    updateUserConfig({ triggerMode: mode });
+  }, []);
 
   return (
     <div className="container">
       <form>
         <fieldset onChange={onTriggerModeChange}>
           <legend>Trigger Mode</legend>
-          {Object.entries(TRIGGER_MODES).map(([value, label]) => {
-            return (
-              <label htmlFor={value} key={value}>
-                <input
-                  type="radio"
-                  id={value}
-                  name="triggerMode"
-                  value={value}
-                  checked={triggerMode === value}
-                />
-                {label}
-              </label>
-            )
-          })}
+          <input value={"Some prompt to chat gpt"} placeholder={""} />
+          <button type="submit">Submit</button>
         </fieldset>
+
+        {/*<SpeechInput/>*/}
       </form>
     </div>
-  )
+  );
 }
 
-export default Popup
+export default Popup;
